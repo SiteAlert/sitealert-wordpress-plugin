5ff5870fbefe3fc73a8531ba0edef70d37f6ba3a
Frank Corso <frank@mylocalwebstop.com>
2016-11-02 23:19:23 -0400

    Adds a delete transient step in uninstall

--- uninstall.php
+++ uninstall.php
@@ -4,9 +4,13 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
+// Deletes plugin's settings
 delete_option( 'wphc_review_message_trigger' );
 delete_option( 'wphc-settings' );
 delete_option( 'wphc_tracker_last_time' );
 delete_option( 'wphc-tracking-notice' );
+
+// Deletes transients from checks (the ones we can delete). The others will self-delete once expired.
+delete_transient( 'wphc_supported_plugin_check' );
 
 ?>
