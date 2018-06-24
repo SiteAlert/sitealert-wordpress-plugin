9acc74a840a7aaa555284b4d956d845fd3aa8f5b
Frank Corso <frank@frankcorso.me>
2018-06-24 16:54:57 -0400

    Changes message concerning file editor

--- php/class-wphc-checks.php
+++ php/class-wphc-checks.php
@@ -100,7 +100,7 @@ class WPHC_Checks {
 		if ( defined( 'DISALLOW_FILE_EDIT' ) && DISALLOW_FILE_EDIT ) {
 			return $this->prepare_array( 'The file editor on this site has been disabled. Great!', 'good' );
 		} else {
-			return $this->prepare_array( 'The file editor on this site has not been disabled. Right now, an admin user can edit plugins and themes from within the WordPress admin. It is recommended to disable file editing within the WordPress dashboard. To do so requires editing the wp-config file as <a href="https://codex.wordpress.org/Hardening_WordPress#Disable_File_Editing" target="_blank">shown here</a>.', 'okay' );
+			return $this->prepare_array( 'The file editor on this site has not been disabled. Right now, an admin user can edit plugins and themes from within the WordPress admin. It is recommended to disable file editing within the WordPress dashboard. Many security plugins, such as iThemes Security, has features to disable the file editor. Alternatively, you can edit the wp-config file <a href="https://codex.wordpress.org/Hardening_WordPress#Disable_File_Editing" target="_blank">shown here</a>.', 'okay' );
 		}
 	}
 
