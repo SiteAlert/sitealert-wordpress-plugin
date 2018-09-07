64d3269964b54ae0264d912ec0f88edf976616a3
Frank Corso <frank@frankcorso.me>
2018-09-07 12:41:21 -0400

    Prepares for release

--- mlw_health_check.php
+++ mlw_health_check.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: WP Health (Formerly My WP Health Check)
  * Description: Check the health of your WordPress installation!
- * Version: 1.6.7
+ * Version: 1.6.8
  * Author: Frank Corso
  * Author URI: https://wphealth.app/
  * Plugin URI: https://wphealth.app/
  * Text Domain: my-wp-health-check
  *
  * @author Frank Corso
- * @version 1.6.7
+ * @version 1.6.8
  * @package WPHC
  */
 
@@ -31,7 +31,7 @@ class My_WP_Health_Check {
 	 * @var string
 	 * @since 1.6.0
 	 */
-	public $version = '1.6.7';
+	public $version = '1.6.8';
 
 	/**
 	 * Main construct
