8aa863e6e125688c2ef5acce4b713d6a66c6824a
Frank Corso <frank@frankcorso.me>
2019-03-08 14:14:02 -0500

    Adds new ad that mentions the new Slack integration

--- php/admin/ads.php
+++ php/admin/ads.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function wphc_generate_ads() {
 	if ( ! isset( $settings['api_key'] ) || empty( $settings['api_key'] ) ) {
 		$ad_message = 'Monitor your WordPress sites to ensure they stay up, healthy, and secure. Check out our premium plans that include uptime monitoring and a central dashboard! <a target="_blank" href="http://bit.ly/2oqLaoR">Learn more!</a>';
-		$ad_number  = rand( 0, 3 );
+		$ad_number  = rand( 0, 4 );
 		switch ( $ad_number ) {
 			case 0:
 				// Ad 1.
@@ -31,12 +31,17 @@ function wphc_generate_ads() {
 
 			case 2:
 				// Ad 3.
-				$ad_message = 'Receive these checks in a weekly email by upgrading to our premium version. <a target="_blank" href="http://bit.ly/2IVBLzh">Learn more!</a>';
+				$ad_message = 'Receive these checks in an email by upgrading to our premium version. <a target="_blank" href="http://bit.ly/2IVBLzh">Learn more!</a>';
 				break;
 
 			case 3:
 				// Ad 4.
 				$ad_message = 'Do not leave your visitors confused! Be notified of any broken images and links on your site by upgrading to our premium version. <a target="_blank" href="http://bit.ly/2RdvAto">Learn more!</a>';
+				break;
+
+			case 4:
+				// Ad 5.
+				$ad_message = 'Get Slack messages with alerts when something is wrong with your site by upgrading to our premium version. <a target="_blank" href="http://bit.ly/2EQKLno">Learn more!</a>';
 				break;
 
 			default:
