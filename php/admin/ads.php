af8cb370c35b98c86fc7450ec8f9f8bcca458cdc
Frank Corso <frank@frankcorso.me>
2019-03-13 12:02:15 -0400

    Allows ads to be translated

--- php/admin/ads.php
+++ php/admin/ads.php
@@ -20,41 +20,58 @@ function wphc_generate_ads( $settings = false ) {
 		$settings = (array) get_option( 'wphc-settings' );
 	}
 	if ( ! isset( $settings['api_key'] ) || empty( $settings['api_key'] ) ) {
-		$ad_message = 'Monitor your WordPress sites to ensure they stay up, healthy, and secure. Check out our premium plans that include uptime monitoring and a central dashboard! <a target="_blank" href="http://bit.ly/2oqLaoR">Learn more!</a>';
-		$ad_number  = rand( 0, 4 );
+		$ad_number = rand( 0, 4 );
 		switch ( $ad_number ) {
 			case 0:
 				// Ad 1.
-				$ad_message = 'Monitor your WordPress sites to ensure they stay up, healthy, and secure. Check out our premium plans that include uptime monitoring and a central dashboard! <a target="_blank" href="http://bit.ly/2oqLaoR">Learn more!</a>';
+				$ad = array(
+					'msg' => __( 'Monitor your WordPress sites to ensure they stay up, healthy, and secure. Check out our premium plans that include uptime monitoring and a central dashboard!', 'my-wp-health-check' ),
+					'url' => 'http://bit.ly/2oqLaoR',
+				);
 				break;
 
 			case 1:
 				// Ad 2.
-				$ad_message = 'Do not lose time and money! Be notified as soon as your site goes down with uptime monitoring. Check out our premium plan for more details! <a target="_blank" href="http://bit.ly/2Cwjskt">Learn more!</a>';
+				$ad = array(
+					'msg' => __( 'Do not lose time and money! Be notified as soon as your site goes down with uptime monitoring. Check out our premium plan for more details!', 'my-wp-health-check' ),
+					'url' => 'http://bit.ly/2Cwjskt',
+				);
 				break;
 
 			case 2:
 				// Ad 3.
-				$ad_message = 'Receive these checks in an email by upgrading to our premium version. <a target="_blank" href="http://bit.ly/2IVBLzh">Learn more!</a>';
+				$ad = array(
+					'msg' => __( 'Receive these checks in an email by upgrading to our premium version.', 'my-wp-health-check' ),
+					'url' => 'http://bit.ly/2IVBLzh',
+				);
 				break;
 
 			case 3:
 				// Ad 4.
-				$ad_message = 'Do not leave your visitors confused! Be notified of any broken images and links on your site by upgrading to our premium version. <a target="_blank" href="http://bit.ly/2RdvAto">Learn more!</a>';
+				$ad = array(
+					'msg' => __( 'Do not leave your visitors confused! Be notified of any broken images and links on your site by upgrading to our premium version.', 'my-wp-health-check' ),
+					'url' => 'http://bit.ly/2RdvAto',
+				);
 				break;
 
 			case 4:
 				// Ad 5.
-				$ad_message = 'Get Slack messages with alerts when something is wrong with your site by upgrading to our premium version. <a target="_blank" href="http://bit.ly/2EQKLno">Learn more!</a>';
+				$ad = array(
+					'msg' => __( 'Get Slack messages with alerts when something is wrong with your site by upgrading to our premium version.', 'my-wp-health-check' ),
+					'url' => 'http://bit.ly/2EQKLno',
+				);
 				break;
 
 			default:
-				$ad_message = 'Monitor your WordPress sites to ensure they stay up, healthy, and secure. Check out our premium plans that include uptime monitoring and a central dashboard! <a target="_blank" href="http://bit.ly/2oqLaoR">Learn more!</a>';
+				$ad = array(
+					'msg' => __( 'Monitor your WordPress sites to ensure they stay up, healthy, and secure. Check out our premium plans that include uptime monitoring and a central dashboard!', 'my-wp-health-check' ),
+					'url' => 'http://bit.ly/2oqLaoR',
+				);
 				break;
 		}
 		?>
 		<div class="wphc-ad">
-			<?php echo $ad_message; ?>
+			<?php echo esc_html( $ad['msg'] ); ?> <a href="<?php echo esc_attr( $ad['url'] ); ?>"><?php esc_html_e( 'Learn more!', 'my-wp-health-check' ); ?></a>
 		</div>
 		<?php
 	}
