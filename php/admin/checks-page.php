32fec43c0be0d3be57d9810047ad65f00a502244
alexposlushnyak <contact.alexander.poslushnyak@gmail.com>
2023-08-18 12:45:13 +0300

    added ajax nonce (security update)

--- php/admin/checks-page.php
+++ php/admin/checks-page.php
@@ -21,6 +21,10 @@ function wphc_generate_checks_page() {
 	global $my_wp_health_check;
 	wp_enqueue_style( 'wphc-style', plugins_url( '../../css/main.css', __FILE__ ), array(), $my_wp_health_check->version );
 	wp_enqueue_script( 'wphc-admin-script', plugins_url( '../../js/wphc-admin.js', __FILE__ ), array( 'backbone', 'underscore', 'wp-util' ), $my_wp_health_check->version );
+	
+	wp_localize_script('wphc-admin-script', 'wphc', [
+			'ajax_nonce' => wp_create_nonce('wphc-nonce'),
+	]);
 
 	$settings = (array) get_option( 'wphc-settings' );
 	?>
